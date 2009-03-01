a00f89d9872b2bf32f6c353af35a19338dc1abca
Michael Marineau <marineam@dubbel.dyn.128.marineau.org>
2009-02-28 23:05:38 -0500

    split out fft blocks

--- monitor.c
+++ monitor.c
@@ -32,6 +32,12 @@ const int BAR_RANGE[] =
     "alsa_output.pci_8086_3a3e_sound_card_0_alsa_playback_0.monitor"
 #define IMON_DEV "/dev/lcd0"
 
+struct fft_context {
+    float *input;
+    float *output;
+    fftwf_plan plan;
+};
+
 char imon_char(float level, int row) {
     const char bars[2][16] = {
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7},
@@ -46,27 +52,62 @@ char imon_char(float level, int row) {
     return bars[row][i];
 }
 
+void fft_init(struct fft_context *context)
+{
+    /* fftw_malloc ensures that the buffers are properly aligned for SSE */
+    context->input = fftwf_malloc(BUF_SIZE);
+    context->output = fftwf_malloc(BUF_SIZE);
+    assert(context->input && context->output);
+
+    /* The data will be returned in half complex format (see fftw docs) */
+    context->plan = fftwf_plan_r2r_1d(BUF_SAMPLES,
+            context->input, context->output,
+            FFTW_R2HC, FFTW_MEASURE);
+}
+
+void fft_free(struct fft_context *context)
+{
+    fftwf_destroy_plan(context->plan);
+    fftwf_free(context->input);
+    fftwf_free(context->output);
+}
+
+void fft_compute(struct fft_context *context, char display[2][BAR_COUNT])
+{
+    /* Apply a Hamming window to focus on the center of this data set */
+    for (int i = 0; i < BUF_SAMPLES; i++)
+        context->input[i] *= (0.54 - 0.46 * cosf((2*M_PI*i)/BUF_SAMPLES));
+
+    fftwf_execute(context->plan);
+
+    for (int i = 0; i < BAR_COUNT; i++) {
+        float max = 0;
+
+        for (int j = BAR_RANGE[i]; j < BAR_RANGE[i+1]; j++) {
+            float mag = sqrt(pow(context->output[j],2) +
+                    pow(context->output[BUF_SAMPLES-j],2));
+            if (mag > max)
+                max = mag;
+        }
+
+        display[0][i] = imon_char(max, 0);
+        display[1][i] = imon_char(max, 1);
+    }
+}
+
 int main(int argc, char * argv[])
 {
     int imon, error, ret = 1;
     pa_simple *pulse;
     pa_sample_spec spec;
-    float *input, *output;
-    fftwf_plan plan;
+    struct fft_context context;
+
+    fft_init(&context);
 
     /* To make the fft easy get data as mono and in floats */
     spec.format = PA_SAMPLE_FLOAT32NE;
     spec.channels = 1;
     spec.rate = SAMPLE_RATE;
-
-    /* fftw_malloc ensures that the buffers are properly aligned for SSE */
-    input = fftwf_malloc(BUF_SIZE);
-    output = fftwf_malloc(BUF_SIZE);
-    assert(input && output);
-
-    /* The data will be returned in half complex format (see fftw docs) */
-    plan = fftwf_plan_r2r_1d(BUF_SAMPLES, input, output,
-            FFTW_R2HC, FFTW_MEASURE);
 
     if (!(pulse = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, PULSE_DEV,
                     "pretty lcd thingy", &spec, NULL, NULL, &error))) {
@@ -82,29 +123,12 @@ int main(int argc, char * argv[])
     for (;;) {
         char display[2][16];
 
-        if (pa_simple_read(pulse, input, BUF_SIZE, &error) < 0) {
+        if (pa_simple_read(pulse, context.input, BUF_SIZE, &error) < 0) {
             fprintf(stderr, "pa_simple_read() failed: %s\n", pa_strerror(error));
             goto finish;
         }
 
-        /* Apply a Hamming window to focus on the center of this data set */
-        for (int i = 0; i < BUF_SAMPLES; i++)
-            input[i] = (0.54 - 0.46 * cosf((2*M_PI*i)/BUF_SAMPLES)) * input[i];
-
-        fftwf_execute(plan);
-
-        for (int i = 0; i < BAR_COUNT; i++) {
-            float max = 0;
-
-            for (int j = BAR_RANGE[i]; j < BAR_RANGE[i+1]; j++) {
-                float mag = sqrt(pow(output[j],2) + pow(output[BUF_SAMPLES-j],2));
-                if (mag > max)
-                    max = mag;
-            }
-
-            display[0][i] = imon_char(max, 0);
-            display[1][i] = imon_char(max, 1);
-        }
+        fft_compute(&context, display);
 
         if (write(imon, display, sizeof(display)) < 0) {
             fprintf(stderr, "write() failed: %s\n", strerror(errno));
@@ -119,8 +143,6 @@ finish:
         close(imon);
     if (pulse)
         pa_simple_free(pulse);
-    fftwf_destroy_plan(plan);
-    fftwf_free(input);
-    fftwf_free(output);
+    fft_free(&context);
     return ret;
 }
